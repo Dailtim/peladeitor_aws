a0c8df94e79533646980dd25abde7d816cf17abf
Dailton Souza <dailtim@gmail.com>
2021-03-23 22:57:21 -0300

    Testando atualização do servidor

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send('Meu backend!')
 })
 
+app.get('/atualizou', (req, res) => {
+    res.status(200).send('Meu backend atualizado!')
+})
+
 app.listen(3000, () => {
     console.log('Backend executando...')
 })
